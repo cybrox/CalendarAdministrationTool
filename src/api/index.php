b45f4e120d62cc16635d135b675bb4f26276c423
CybroX <marc@cybrox.eu>
2013-09-26 13:30:36 +0200

    CAT v0.0.1
    Bibliotheken und externe Scripts eingebunden, Entwicklung der
    Benutzeroberfläche vorbereitet.

--- src/api/index.php
+++ src/api/index.php
@@ -1,5 +1,20 @@
 <?php
 
+/* CAT - Calendar Administration Tool
+** Datenbankschnittstelle http/https
+**
+** Autor: Sven Gehring
+**
+** Informationen zur Lizenz dieses Quellcodes
+** finden sie in der beiliegenden LICENSE.md
+**
+** Für weitere Informationen zum Aufbau und der
+** Verwendung dieser Schnittstelle, lesen sie
+** bitte die API-Dokumentation, zu finden unter
+** Link:  "http://cybrox.eu/project/cat/doc/"
+** Datei: "#04 - Datenbankschnittstelle.docx"
+*/
+
 	include('./system/config.php');
 	include('./system/interfaces/apielement.interface.php');
 	include('./system/classes/catinterface.class.php');
