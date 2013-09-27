a81b2170646ea71cbc3c2ca1adfcbe2ff2800875
CybroX <marc@cybrox.eu>
2013-09-27 08:41:20 +0200

    CAT v0.0.6
    Erste Version der Fehlerverarbeitung und Seitenimplementation +
    Cookieunterstützung

--- src/js/core.js
+++ src/js/core.js
@@ -9,6 +9,89 @@
 
 
 /**
+ * Benutzerobjekt
+ */
+var user = {
+	'id':     0,
+	'level':  0,
+	'active': 0,
+	'name':   "",
+	'email':  "",
+	'token':  ""
+}
+
+
+
+/**
+ * Seiteninitialisierung
+ *
+ * Wird beim Aufruf der Seite verwendet um
+ * uU eine Anmeldeabfrage anzuzeigen oder den
+ * Benutzer-Token aus eine cookie zu lesen.
+ */
+$(document).ready(function(){
+	if($.cookie("cat_user") == undefined){
+		
+		$.loader('show');
+		$.popups('login');
+		$.loader('hide');
+		
+	} else {
+		$('nav').css("left", "0");
+		
+		usercookie = $.cookie("cat_user");
+		userparts  = usercookie.split("-");
+		user.token = userparts[0];
+		user.id    = userparts[1];
+		
+	}
+});
+ 
+
+/**
+ * Fehleranzeige zur Benutzerinformation
+ *
+ * Bei einem AJAX Fehler wird dem Benutzer
+ * automatisch eine Fehlermeldung ausgegeben
+ */
+$.error = function(message){
+
+	$('#errormsg').text(message);
+	$('#errors').fadeIn();
+
+}
+ 
+ 
+/**
+ * Popups zur Darstellung von On-Site Formularen
+ *
+ * Alle Aktionen im Kalendertool werden direkt
+ * auf der Seite ausgeführt auf der sie aufgerufen
+ * wurden, indem sie in ein Popup ausgelagert werden.
+ */
+$.popups = function(content){
+	
+	if(content != "close"){
+		$.ajax({
+			type: 'GET',
+			url: './src/form/'+content+'.html',
+			success: function(data){
+				$('#popups').html(data);
+			},
+			error: function() {
+				$.error("Formular \""+content+"\" konnte nicht geladen werden");
+			}
+		});
+		$('#popups').show('slow');
+	} else {
+		$('#popups').hide('slow');
+		$('#popups').html("");
+	}
+	
+}
+ 
+ 
+/**
  * AJAX Loader für Ladeanzeige
  *
  * Beim laden einer neuen Seite oder neuen
@@ -27,6 +110,8 @@ $.loader = function(action){
 	}
 }
 
+
+
 /**
  * Linkfunktionen ersetzen
  *
@@ -36,10 +121,10 @@ $('a').click(function(e){
 	
 	$.loader('show');
 	
-	link  = $(this).attr('href');
-	parts = link.split('_');
-	type  = parts[0];
-	outpt = parts[1];
+	link   = $(this).attr('href');
+	parts  = link.split('_');
+	type   = parts[0];
+	target = parts[1];
 	
 	switch(type){
 		case "page":
@@ -48,16 +133,19 @@ $('a').click(function(e){
 			
 			$.ajax({
 				type: 'GET',
-				url: './src/page/'+outpt+'.html',
+				url: './src/page/'+target+'.html',
 				success: function(data){
-					$('#content').html(data);
+					$('#contentInner').html(data);
 					
 					$.loader('hide');
 				},
-				error: function(xhr, status, error) {
-					alert(error);
+				error: function() {
+					$.error("Seiteninhalt \""+target+"\" konnte nicht geladen werden");
 				}
 			});
+			break;
+		case "popup":
+			$.popup(target);
 			break;
 	}
 	
@@ -77,8 +165,6 @@ $('#userfieldVisible').click(function(){
 	
 	$('#userfieldIcon').toggleClass('userfieldIconRotate');
 });
-
-
 
 
 
